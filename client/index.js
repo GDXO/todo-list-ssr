f9749a2e14a114db12f81a14a6c74618f8f7b850
lcy668 <liuchaoyanzi1103@163.com>
2024-02-25 08:50:49 +0800

    chore: 添加 Vuex

--- client/index.js
+++ client/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 import App from './App.vue'
 import '@/assets/styles/global.styl'
 import createRouter from '@/config/router/'
-import store from '@/config/store/'
+import createStore from '@/config/store/'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 // 定义 router
 const router = createRouter()
+
+// 定义 store
+const store = createStore()
 
 new Vue({
   router,
